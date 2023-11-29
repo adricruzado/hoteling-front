a528d26da885434818076ed2294cf51f3ad99cf0
Adri Cruzado <adricruzado96@gmail.com>
2023-11-29 09:34:52 +0100

    Add App component styles

---
+++ src/components/App/AppStyled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+  align-items: center;
+  width: 100%;
+`;
+
+export default AppStyled;

aec1c9e22b3faeb888a217b27463ba851751a76b
Adri Cruzado <adricruzado96@gmail.com>
2023-11-29 13:54:08 +0100

    Add Navigation component styles

---
+++ src/components/Navigation/NavigationStyled.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const NavigationStyled = styled.nav`
+  .navigation {
+    display: flex;
+    justify-content: center;
+    gap: 60px;
+    font-family: ${({ theme }) => theme.typography.secondaryFont};
+    font-size: 25px;
+    font-weight: 500;
+    text-transform: uppercase;
+    margin-top: 20px;
+    background-color: ${({ theme }) => theme.colors.accent};
+    color: ${({ theme }) => theme.colors.light};
+    margin-bottom: 15px;
+    padding: 20px;
+    border-radius: 40px;
+    width: 370px;
+  }
+
+  .active {
+    text-decoration: underline;
+  }
+`;
+
+export default NavigationStyled;

22bf071e51253de04251608336b953b74f293ec6
Adri Cruzado <adricruzado96@gmail.com>
2023-11-29 15:41:09 +0100

    Add HotelsPage page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "../Header/Header";
 import AppStyled from "./AppStyled";
+import HotelsPage from "../../pages/HotelsPage/HotelsPage";
 
 const App = (): React.ReactElement => {
   return (
@@ -9,7 +10,7 @@ const App = (): React.ReactElement => {
       <main>
         <Routes>
           <Route path="/" element={<Navigate to="/hotels/" />} />
-          <Route path="/hotels" />
+          <Route path="/hotels" element={<HotelsPage />} />
         </Routes>
       </main>
     </AppStyled>
---
+++ src/pages/HotelsPage/HotelsPage.tsx
@@ -0,0 +1,11 @@
+import HotelsPageStyled from "./HotelsPageStyled";
+
+const HotelsPage = (): React.ReactElement => {
+  return (
+    <HotelsPageStyled>
+      <h1 className="page-title">Your visited hotels:</h1>
+    </HotelsPageStyled>
+  );
+};
+
+export default HotelsPage;

8f7ad0997320a69905325dd0686e84fff959962e
Adri Cruzado <adricruzado96@gmail.com>
2023-11-30 10:13:06 +0100

    Add HotelCard component

--- src/pages/HotelsPage/HotelsPage.tsx
+++ src/pages/HotelsPage/HotelsPage.tsx
@@ -1,9 +1,12 @@
+import HotelCard from "../../components/HotelCard/HotelCard";
+import { hotelData } from "../../data/hotel/hotelData";
 import HotelsPageStyled from "./HotelsPageStyled";
 
 const HotelsPage = (): React.ReactElement => {
   return (
     <HotelsPageStyled>
       <h1 className="page-title">Your visited hotels:</h1>
+      <HotelCard hotel={hotelData} />
     </HotelsPageStyled>
   );
 };

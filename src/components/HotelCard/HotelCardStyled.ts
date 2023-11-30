69fe0ca9f12a4a99528682ed084a9a901f77d325
Adri Cruzado <adricruzado96@gmail.com>
2023-11-30 16:05:54 +0100

    Update HotelCard component

--- src/components/HotelCard/HotelCardStyled.ts
+++ src/components/HotelCard/HotelCardStyled.ts
@@ -9,17 +9,12 @@ const HotelCardStyled = styled.article`
   color: #fff;
   border-radius: 30px;
   width: 340px;
-  height: 530px;
+  height: 100%;
   box-shadow: 0 0 15px ${({ theme }) => theme.colors.accent};
   padding: 20px;
   margin-bottom: 30px;
 
   .hotel-card {
-    &__info {
-      display: flex;
-      flex-direction: column;
-    }
-
     &__name {
       font-size: 30px;
       font-weight: 600;
@@ -34,7 +29,7 @@ const HotelCardStyled = styled.article`
     &__details {
       display: flex;
       align-items: center;
-      font-size: 22px;
+      font-size: 19px;
       gap: 10px;
       margin-bottom: 8px;
     }

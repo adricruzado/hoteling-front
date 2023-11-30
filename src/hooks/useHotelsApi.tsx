9cf153f1c7859a2f70706ca6564f3b34657810c9
Adri Cruzado <adricruzado96@gmail.com>
2023-11-30 20:37:51 +0100

    Add useHotelsApi custom hook test

--- src/hooks/useHotelsApi.tsx
+++ src/hooks/useHotelsApi.tsx
@@ -6,7 +6,9 @@ const useHotelsApi = () => {
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
   const getHotels = useCallback(async () => {
-    const { data: hotels } = await axios.get<HotelStructure[]>("/hotels");
+    const { data: hotels } = await axios.get<{ hotels: HotelStructure[] }>(
+      "/hotels",
+    );
 
     return hotels;
   }, []);

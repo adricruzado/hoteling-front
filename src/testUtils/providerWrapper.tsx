ba33a4b2920004c76e93e89128eca98716b45c1f
Adri Cruzado <adricruzado96@gmail.com>
2023-12-02 12:40:54 +0100

    Add uiReducer test

---
+++ src/testUtils/providerWrapper.tsx
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { hotelsReducer } from "../store/features/hotels/hotelsSlice";
+import { uiReducer } from "../store/features/ui/uiSlice";
+import { apiHotelsMock } from "../mocks/apiHotelsMock";
+import { Provider } from "react-redux";
+import { PropsWithChildren } from "react";
+
+const getMockStore = () => {
+  const mockStore = configureStore({
+    reducer: { hotelsState: hotelsReducer, uiState: uiReducer },
+    preloadedState: {
+      hotelsState: { hotels: apiHotelsMock },
+      uiState: { isLoading: false },
+    },
+  });
+
+  return mockStore;
+};
+
+export const providerWrapper = ({ children }: PropsWithChildren) => {
+  const mockStore = getMockStore();
+
+  return <Provider store={mockStore}>{children}</Provider>;
+};

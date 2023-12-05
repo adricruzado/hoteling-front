ef57e9632e040f319ca4081f6dd73a393f07e3b7
Adri Cruzado <adricruzado96@gmail.com>
2023-12-05 18:17:54 +0100

    Fix errorHandler

--- src/mocks/errorHandlers.ts
+++ src/mocks/errorHandlers.ts
@@ -1,9 +1,6 @@
 import { http, HttpResponse } from "msw";
 
 export const errorHandlers = [
-  http.get(`${import.meta.env.VITE_API_URL}/hotels`, () =>
-    HttpResponse.error(),
-  ),
   http.delete(
     `${import.meta.env.VITE_API_URL}/hotels/656492010f2c29b15944b0d8`,
     () => HttpResponse.error(),

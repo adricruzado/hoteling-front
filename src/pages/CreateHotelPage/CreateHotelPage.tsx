61e465314544311afe4e015de1c1bdf73cba7793
Adri Cruzado <adricruzado96@gmail.com>
2023-12-07 09:20:40 +0100

    Add HotelForm component test

--- src/pages/CreateHotelPage/CreateHotelPage.tsx
+++ src/pages/CreateHotelPage/CreateHotelPage.tsx
@@ -1,9 +1,11 @@
+import HotelForm from "../../components/HotelForm/HotelForm";
 import CreateHotelPageStyled from "./CreateHotelPageStyled";
 
 const CreateHotelPage = (): React.ReactElement => {
   return (
     <CreateHotelPageStyled>
       <h1 className="page-title">Add your hotel:</h1>
+      <HotelForm />
     </CreateHotelPageStyled>
   );
 };

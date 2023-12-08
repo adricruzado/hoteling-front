8e35b41a0dc41146c905ebea3cd83e57ca71bc32
Adri Cruzado <adricruzado96@gmail.com>
2023-12-08 19:22:45 +0100

    Add addHotel function to useHotelsApi

--- src/hooks/useHotelsApi.tsx
+++ src/hooks/useHotelsApi.tsx
@@ -1,15 +1,19 @@
 import axios from "axios";
 import { useCallback } from "react";
-import { HotelStructure } from "../store/features/hotels/types";
+import { HotelStructure, HotelWithoutId } from "../store/features/hotels/types";
 import { useAppDispatch } from "../store/hooks";
 import {
   hideLoadingActionCreator,
   showLoadingActionCreator,
 } from "../store/features/ui/uiSlice";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { addHotelActionCreator } from "../store/features/hotels/hotelsSlice";
 
 const useHotelsApi = () => {
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+  const navigate = useNavigate();
 
   const dispatch = useAppDispatch();
 
@@ -56,7 +60,40 @@ const useHotelsApi = () => {
     [dispatch],
   );
 
-  return { getHotels, deleteHotel };
+  const addHotel = useCallback(
+    async (newHotel: HotelWithoutId): Promise<HotelWithoutId | undefined> => {
+      try {
+        dispatch(showLoadingActionCreator());
+
+        const {
+          data: { hotel },
+        } = await axios.post<{ hotel: HotelStructure }>(
+          "/hotels/create",
+          newHotel,
+        );
+
+        dispatch(addHotelActionCreator(hotel));
+
+        toast.success("Great! The hotel has been created.", {
+          theme: "colored",
+        });
+
+        dispatch(hideLoadingActionCreator());
+
+        navigate("/");
+
+        return hotel;
+      } catch {
+        dispatch(hideLoadingActionCreator());
+
+        toast.error("Sorry! We couldn't create the hotel.", {
+          theme: "colored",
+        });
+      }
+    },
+    [dispatch, navigate],
+  );
+  return { getHotels, deleteHotel, addHotel };
 };
 
 export default useHotelsApi;

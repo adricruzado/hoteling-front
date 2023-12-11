4aaa8f30f100c2ba01c789d11f0b827d14c2bda4
Adri Cruzado <adricruzado96@gmail.com>
2023-12-11 10:27:34 +0100

    Add ModifyHotelPage page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import CreateHotelPage from "../../pages/CreateHotelPage/CreateHotelPage";
 import HotelDetailsPage from "../../pages/HotelDetailsPage/HotelDetailsPage";
+import ModifyHotelPage from "../../pages/ModifyHotelPage/ModifyHotelPage";
 
 const App = (): React.ReactElement => {
   const uiState = useAppSelector((state) => state.uiState);
@@ -23,6 +24,7 @@ const App = (): React.ReactElement => {
           <Route path="/hotels" element={<HotelsPage />} />
           <Route path="/create" element={<CreateHotelPage />} />
           <Route path="/hotels/:hotelId" element={<HotelDetailsPage />} />
+          <Route path="/hotels/:hotelId/modify" element={<ModifyHotelPage />} />
           <Route path="/*" element={<NotFoundPage />} />
         </Routes>
       </main>

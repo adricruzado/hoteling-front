61116b4f59f5b01a50342de9b73fa4281f92bb16
Adri Cruzado <adricruzado96@gmail.com>
2023-12-13 14:55:23 +0100

    Add useEffect to ModifyHotelPage

--- src/pages/ModifyHotelPage/ModifyHotelPage.tsx
+++ src/pages/ModifyHotelPage/ModifyHotelPage.tsx
@@ -1,16 +1,24 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import HotelForm from "../../components/HotelForm/HotelForm";
 import useHotelsApi from "../../hooks/useHotelsApi";
-import { modifyHotelActionCreator } from "../../store/features/hotels/hotelsSlice";
-import { HotelWithoutId } from "../../store/features/hotels/types";
+import {
+  loadSelectedHotelActionCreator,
+  modifyHotelActionCreator,
+} from "../../store/features/hotels/hotelsSlice";
+import {
+  HotelStructure,
+  HotelWithoutId,
+} from "../../store/features/hotels/types";
 import ModifyHotelPageStyled from "./ModifyHotelPageStyled";
+import { useEffect } from "react";
 
 const ModifyHotelPage = (): React.ReactElement => {
-  const { modifyHotel } = useHotelsApi();
+  const { modifyHotel, loadSelectedHotel } = useHotelsApi();
   const { selectedHotel } = useAppSelector((state) => state.hotelsState);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const { hotelId } = useParams();
 
   const updateHotel = async (hotel: HotelWithoutId) => {
     const updatedHotel = await modifyHotel(selectedHotel._id, hotel);
@@ -18,6 +26,16 @@ const ModifyHotelPage = (): React.ReactElement => {
     dispatch(modifyHotelActionCreator(updatedHotel!));
     navigate("/hotels");
   };
+
+  useEffect(() => {
+    (async () => {
+      const hotel = await loadSelectedHotel(hotelId as string);
+
+      dispatch(loadSelectedHotelActionCreator(hotel as HotelStructure));
+
+      return hotel;
+    })();
+  }, [dispatch, loadSelectedHotel, hotelId]);
 
   return (
     <ModifyHotelPageStyled>
